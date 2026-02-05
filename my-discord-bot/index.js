e89683ece6f704b9f8be56948661d267725eb22d
Jeremy Hiers <jjhiers@comcast.net>
2026-02-05 10:12:59 -0500

    Added the basic discord bot files and folders including node modules. Started the work on the bot code.

---
+++ my-discord-bot/index.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits } = require('discord.js'); 
+require('dotenv').config(); 
+
+const client = new Client({ 
+  intents: [ 
+      GatewayIntentBits.Guilds,  
+      GatewayIntentBits.GuildMessages,  
+      GatewayIntentBits.MessageContent] 
+});
